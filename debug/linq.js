69b76717bd6ad51c6b5a51388cc4954ffe9e2c02
曾大海 <zsea@qq.com>
2018-09-04 17:08:55 +0800

    添加order by 支持

--- debug/linq.js
+++ debug/linq.js
@@ -2,3 +2,8 @@ var linq=require('../lib/linq');
 
 var q=new linq('db');
 console.log(q.table('tb').where(p=>p.x==1&&p.y==7).select(p=>{p.x,p.y}).toSql());
+console.log('=============');
+var db=new linq('db').table('tb').where(p=>p.x==1&&p.y==7).select(p=>p.x);
+console.log(new linq('db').table('tb').leftJoin(db).on((q,p)=>q.x==p.x).where((q,p)=>p.x==1&&p.y==7).select(p=>{p.x,p.y}).toSql());
+console.log('=============');
+console.log(new linq('db').table('tb').where(p=>p.x==1&&p.y==7).orderBy(p=>p.x).thenByDescending(p=>p.y).select(p=>p.x).toSql())
