f0f49d3dfe5c2218187024371f683fa9a51c88f6
zsea.home <zsea@home>
2020-02-25 01:31:27 +0800

    each增加错误抛出

---
+++ test/index.js
@@ -0,0 +1,21 @@
+const Linq = require("../lib/")
+    , db = new Linq('mysql://root@127.0.0.1/seller?connectionLimit=2');
+async function main() {
+    try {
+        var count = await db.table("agrees").where(p => p.id == 1).each(1, async function () {
+            console.log(arguments);
+            var x = 2 / 0;
+            console.log(x);
+            throw new Error("aaa");
+            //await db.table("agrees").first();
+        })
+    }
+    catch (e) { 
+        console.log(e);
+    }
+    console.log("总数", count);
+}
+main();
+//main();
+//main();
+//main();
