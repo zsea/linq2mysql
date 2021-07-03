5957c5daa12a72319688a8daf0624efb3ee1e7d0
zsea <zsea@qq.com>
2021-07-04 02:29:42 +0800

    当Bigint字段为null时，转换出错

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const Linq = require("../lib/")
     , db = new Linq('mysql://root@127.0.0.1/seller?connectionLimit=1&supportBigNumbers=true');
 async function main() {
-    let data=await db.table("agrees").where(p => p.id == 1).toArray();
+    let data=await db.table("agrees").toArray();
     console.log(data);
     process.exit();
 }
