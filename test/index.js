7d61abb73d391fe427dd1cf077795770cfc84d2e
zsea <zsea@qq.com>
2021-07-04 14:43:14 +0800

    修复Bigint类型转换时出错

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const Linq = require("../lib/")
-    , db = new Linq('mysql://root@127.0.0.1/seller?connectionLimit=1&supportBigNumbers=true');
+    , db = new Linq('mysql://root@127.0.0.1/eshop?connectionLimit=1&supportBigNumbers=true');
 async function main() {
-    let data=await db.table("agrees").toArray();
+    let data=await db.table("items").toArray();
     console.log(data);
     process.exit();
 }
