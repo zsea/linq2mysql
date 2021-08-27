7d3573f2f15e9a8e1d3770622b441619ea8d1cb0
曾大海 <zsea@qq.com>
2021-08-27 21:37:09 +0800

    where函数中，若传入的对象字段值为数组，则使用in语句进行查询

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const Linq = require("../lib/")
     , db = new Linq('mysql://root@127.0.0.1/eshop?connectionLimit=1&supportBigNumbers=true');
 async function main() {
-    let data=await db.table("items").toArray();
+    let data=await db.table("items").where({sales:[1,2,3,4,5],status:"ONSALE"}).toArray();
     console.log(data);
     process.exit();
 }
